f259819a8c4ac69fc059a86e536f0d02be95ca30
beloveddd <yuliya.bialova@gmail.com>
2023-09-27 15:07:03 +0300

    feat: use Angular material components for EventRegistrationComponent

--- src/app/components/attendee-list/attendee-list.component.ts
+++ src/app/components/attendee-list/attendee-list.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'attendee-list',
   standalone: true,
   templateUrl: './attendee-list.component.html',
-  styleUrls: ['./attendee-list.component.scss']
+  styleUrls: ['./attendee-list.component.scss'],
 })
 export class AttendeeListComponent {
 
--- src/app/components/event-registration/event-registration.component.ts
+++ src/app/components/event-registration/event-registration.component.ts
@@ -1,12 +1,18 @@
 import { Component, inject } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatInputModule}  from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+
 
 @Component({
   selector: 'event-registration',
   standalone: true,
   templateUrl: './event-registration.component.html',
   styleUrls: ['./event-registration.component.scss'],
-  imports: [ ReactiveFormsModule ],
+  imports: [ ReactiveFormsModule, FormsModule, MatFormFieldModule, MatInputModule, MatSelectModule, MatDatepickerModule, MatNativeDateModule ],
 })
 export class EventRegistrationComponent {
   registrationForm!: FormGroup;
