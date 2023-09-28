e029da3f6f2d979d415e7ffef7233cd1dcffbe0f
beloveddd <yuliya.bialova@gmail.com>
2023-09-28 11:31:34 +0300

    feat: add sorting for table with users

--- src/app/components/app-header/app-header.component.ts
+++ src/app/components/app-header/app-header.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './app-header.component.html',
   styleUrls: ['./app-header.component.scss'],
   standalone: true,
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppHeaderComponent {}
 
--- src/app/components/attendee-list/attendee-list.component.ts
+++ src/app/components/attendee-list/attendee-list.component.ts
@@ -1,6 +1,8 @@
+import { LiveAnnouncer } from '@angular/cdk/a11y';
 import { DatePipe, TitleCasePipe } from '@angular/common';
-import { Component, inject } from '@angular/core';
-import { MatTableModule } from '@angular/material/table';
+import { ChangeDetectionStrategy, Component, ViewChild, inject } from '@angular/core';
+import { MatSort, MatSortModule, Sort } from '@angular/material/sort';
+import { MatTableDataSource, MatTableDataSourcePaginator, MatTableModule } from '@angular/material/table';
 
 import { User } from 'src/app/shared/interfaces/users.interface';
 import { UsersService } from 'src/app/shared/services/users.service';
@@ -9,16 +11,32 @@ import { UsersService } from 'src/app/shared/services/users.service';
   standalone: true,
   templateUrl: './attendee-list.component.html',
   styleUrls: ['./attendee-list.component.scss'],
-  imports: [ MatTableModule, TitleCasePipe, DatePipe ],
+  imports: [ MatTableModule, TitleCasePipe, DatePipe, MatSortModule ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AttendeeListComponent {
   displayedColumns: string[] = ['name', 'email', 'phone', 'registrationType', 'date'];
-  users!: User[];
+  users!: MatTableDataSource<User, MatTableDataSourcePaginator>;
+
+  @ViewChild(MatSort) sort!: MatSort;
 
   private _usersService = inject(UsersService);
+  private _liveAnnouncer = inject(LiveAnnouncer);
 
   ngOnInit() {
     this._usersService.initUsers();
-    this.users = this._usersService.users;
+    this.users = new MatTableDataSource(this._usersService.users);
+  }
+
+  ngAfterViewInit() {
+    this.users.sort = this.sort;
+  }
+
+  announceSortChange(sortState: Sort) {
+    if (sortState.direction) {
+      this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
+    } else {
+      this._liveAnnouncer.announce('Sorting cleared');
+    }
   }
 }
